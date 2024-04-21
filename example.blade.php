34ff13f515eef242758a82fc85207d8b980db7b3
d1ms <dimedrol@li.ru>
2024-04-21 08:45:53 +0300

    Update example.blade.php

--- example.blade.php
+++ example.blade.php
@@ -1,1 +1,3 @@
-test
+<div class="notification-item__img">
+  <img class="image" src="{{ $order->urlThumbnails('avatar', '60x60' , 'fit') }}" width="48" height="48" alt="&" aria-hidden="true" loading="lazy" decoding="async">
+</div>
